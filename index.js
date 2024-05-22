4ff9a623d8d5630a1d27bde9ade169b8db3c0f00
vatsal prajapati <vatsprajapati20@gmail.com>
2024-05-22 11:48:48 +0530

    Coverd for Basic question and answer

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1. Write a JavaScript function to calculate the sum of two numbers.  
+const a = 10;
+const b = 20;
+console.log(a + b)
+// ---------------------------------------------------
+function calculate(a, b) {
+    return a + b;
+}
+
+console.log(calculate(4, 5));
+
+// 2. Write a JavaScript program to find the maximum number in an array. 
+
+const number = [4, 6, 7, 20, 56, 58];
+
+console.log(Math.max(...number));
+// -------------------------------------------------------------
+function maximumNumber(arr) {
+    return Math.max(...arr)
+}
+
+console.log(maximumNumber(number));
+
+// 4. Write a JavaScript program to reverse a given string. 
+
+function reversedString(str) {
+    return str.split('').reverse().join('');
+}
+
+const original = "Hello World!"
+const reversedsstring = reversedString(original);
+
+console.log(reversedsstring)
